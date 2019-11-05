c76354b225ad03ccd52db2e516f32fc677e8d415
sandeepsingh2810 <sandeepjangra0100@gmail.com>
2019-11-05 17:59:59 -0500

    Initial Commit

---
+++ IOS_StaticTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_StaticTableView
+//
+//  Created by Sandeep Jangra on 2019-11-05.
+//  Copyright © 2019 Karan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
